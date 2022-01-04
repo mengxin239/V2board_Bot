b34ae12dcf93d88b0b4baa0ba17bd01f8dc2b182
萌新 <mengxin82@outlook.com>
2022-01-05 00:44:15 +0800

    Update login.php

--- lib/login.php
+++ lib/login.php
@@ -1,3 +1,5 @@
 <?php
+$uname=explode('@',$admin_username);
+$admin_username=$uname[0].'%40'.$uname[1];
 exec("curl -s https://".$hostname."/api/v1/passport/auth/login -X POST -d 'email=".$admin_username."&password=".$admin_password."' -c logined.cookie",$a);
 ?>
